9e844e55f32d3e67a9cfcc690382620f580cd037
jsj188508 <jsj188508@sina.com>
2018-09-07 07:45:55 +0800

    补充0

--- node/00_nodeServer.js
+++ node/00_nodeServer.js
@@ -1,6 +1,17 @@
-var http=require('http');
+var http = require('http');
 // 建立http请求
-http.createServer().listen(8000,function(){
-    console.log('web服务器启动完毕时触发')
-})
+http.createServer(function (req, res) {
+    //设置请求头部
+    res.writeHead(200, { 'Content-type': 'text/html;charset=utf-8' });
+    if (req.url !== "/favicon.ico") { //清楚第二次访问 //url是node自带的
+        console.log('访问中');
+        //想网页中写入内容
+        res.write('欢迎来到node世界');
+        console.log(req.url);
+        //结束http请求 //也可以输出内容 response.end('000')
+        res.end();
+    }
+}).listen(8000,function () {
+    console.log('web服务器监听端口时触发')
+});
 console.log('请运行：http://127.0.0.1:8000');
