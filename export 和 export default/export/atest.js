911a219ba796e4b3d8b62ba9c0415449de13add0
jsj228 <jsj188508@sina.com>
2022-02-17 02:38:30 +0800

    面试

--- export 和 export default/export/atest.js
+++
@@ -1,8 +0,0 @@
-//导出常量
-// export const Aconst = Math.sqrt;
-// //导出函数
-// export function square(x){
-//     return x + x;
-// }
-
-alert('1110')
--- node/userModels.js
+++
@@ -1,9 +0,0 @@
-function userClass(id,name,age) {
-    this.id=id;
-    this.name=name;
-    this.age=age;
-    this.enter=function () {
-        console.log(this.name+'进入userClass类中')
-    }
-};
-module.exports=userClass; 
